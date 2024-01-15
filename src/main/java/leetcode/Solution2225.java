148e344ad55e053b0968ba888d479a23b077e31f
rockintuna <rockintuna@naver.com>
2024-01-15 23:09:29 +0900

    leetcode 2225 배열만 사용하여 성능 개선

--- src/main/java/leetcode/Solution2225.java
+++ src/main/java/leetcode/Solution2225.java
@@ -1,40 +1,43 @@
 package leetcode;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution2225 {
     public List<List<Integer>> findWinners(int[][] matches) {
-        Map<Integer, Integer> playerAndLoseCountMap = new HashMap<>();
-        Set<Integer> playerSet = new HashSet<>();
-
+        Integer[] looseCountArr = new Integer[100000+1];
         for (int i = 0; i < matches.length; i++) {
             int[] match = matches[i];
+
             int winner = match[0];
             int looser = match[1];
-            playerSet.add(winner);
-            playerSet.add(looser);
 
-            if ( playerAndLoseCountMap.containsKey(looser) ) {
-                playerAndLoseCountMap.replace(looser, playerAndLoseCountMap.get(looser) + 1);
+            if ( looseCountArr[winner] == null ) {
+                looseCountArr[winner] = 0;
+            }
+            if ( looseCountArr[looser] == null || looseCountArr[looser] == 0 ) {
+                looseCountArr[looser] = 1;
             } else {
-                playerAndLoseCountMap.put(looser, 1);
+                looseCountArr[looser] = looseCountArr[looser] + 1;
             }
         }
 
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
-        for (Integer i : playerSet) {
-            if ( playerAndLoseCountMap.get(i) == null ) {
+        for (int i = 0; i < looseCountArr.length; i++) {
+            Integer looseCount = looseCountArr[i];
+            if ( looseCount == null ) {
+                continue;
+            }
+            if ( looseCount == 0 ) {
                 list1.add(i);
-            } else if ( playerAndLoseCountMap.get(i) == 1 ) {
+            } else if ( looseCount == 1 ) {
                 list2.add(i);
             }
         }
-
         List<List<Integer>> result = new ArrayList<>(2);
-        result.add(list1.stream().sorted().collect(Collectors.toList()));
-        result.add(list2.stream().sorted().collect(Collectors.toList()));
+        result.add(list1);
+        result.add(list2);
         return result;
     }
 
