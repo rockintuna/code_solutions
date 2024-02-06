1eafee1343203778053b157a44bffd4d9f5a3d85
rockintuna <rockintuna@naver.com>
2024-02-06 23:30:23 +0900

    leatcode 49 sorting String and using map refactoring

--- src/main/java/leetcode/Solution49.java
+++ src/main/java/leetcode/Solution49.java
@@ -5,21 +5,25 @@ import java.util.*;
 public class Solution49 {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        for (int i = 0; i < strs.length; i++) {
-            String str = strs[i];
-            char[] charArray = str.toCharArray();
-            Arrays.sort(charArray);
-            String string = Arrays.toString(charArray);
 
-            if ( map.containsKey(string) ) {
-                map.get(string).add(str);
+        for (String str : strs) {
+            String sortedString = sortString(str);
+
+            if (map.containsKey(sortedString)) {
+                map.get(sortedString).add(str);
             } else {
-                List<String> objects = new ArrayList<>();
-                objects.add(str);
-                map.put(string, objects);
+                List<String> newGroup = new ArrayList<>();
+                newGroup.add(str);
+                map.put(sortedString, newGroup);
             }
         }
 
         return new ArrayList<>(map.values());
+    }
+
+    private static String sortString(String str) {
+        char[] charArray = str.toCharArray();
+        Arrays.sort(charArray);
+        return Arrays.toString(charArray);
     }
 }
