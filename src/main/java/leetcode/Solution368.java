725fa1867a6bc3d662fc9e8766d8a914887a8571
rockintuna <rockintuna@naver.co.kr>
2024-02-09 19:03:01 +0900

    Revert "leetcode 368 by DP"
    This reverts commit 1957fe727dca14e73b3bae37abb6867d7020f6b3.

--- src/main/java/leetcode/Solution368.java
+++ src/main/java/leetcode/Solution368.java
@@ -10,52 +10,49 @@ public class Solution368 {
 
         int size = 0;
 
-        List<List<Integer>> dp = new ArrayList<>();
-        List<Integer> result = null;
+        List<Set<Integer>> dp = new ArrayList<>();
+        Set<Integer> result = null;
 
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length; i++) {
-            List<List<Integer>> copyOfDp = new ArrayList<>(dp);
-            List<Integer> list = null;
-            int maxSize = 0;
+            List<Set<Integer>> copyOfDp = new ArrayList<>(dp);
             for (int j = 0; j < dp.size(); j++) {
-                List<Integer> integers = dp.get(j);
+                Set<Integer> integers = dp.get(j);
                 if ( isDivisible(integers, nums[i]) ) {
-                    if ( integers.size() > maxSize ) {
-                        maxSize = integers.size();
-                        list = integers;
-                    }
+                    Set<Integer> newSet = new HashSet<>(integers);
+                    newSet.add(nums[i]);
+                    copyOfDp.add(newSet);
                 }
             }
-
-            if ( list != null ) {
-                List<Integer> newList = new ArrayList<>(list);
-                newList.add(nums[i]);
-                copyOfDp.add(newList);
-            } else {
-                copyOfDp.add(List.of(nums[i]));
-            }
+            copyOfDp.add(Set.of(nums[i]));
             dp = copyOfDp;
         }
 
-        for (List<Integer> integers : dp) {
+        for (Set<Integer> integers : dp) {
             if ( integers.size() > size ) {
                 size = integers.size();
                 result = integers;
             }
         }
-        return result;
+        return new ArrayList<>(result);
     }
 
-    private boolean isDivisible(List<Integer> list, int num) {
-        return num % list.get(list.size() - 1) == 0;
+    private boolean isDivisible(Set<Integer> set, int num) {
+        boolean isDivisible = true;
+        for (Integer integer : set) {
+            if ( num % integer != 0 ) {
+                isDivisible = false;
+                break;
+            }
+        }
+        return isDivisible;
     }
 
     public static void main(String[] args) {
         Solution368 solution368 = new Solution368();
-        int[] nums = {5,9,18,54,108,540,90,180,360,720};
-//        int[] nums = {1,2,4,8};
+//        int[] nums = {5,9,18,54,108,540,90,180,360,720};
+        int[] nums = {1,2,4,8};
         List<Integer> result = solution368.largestDivisibleSubset(nums);
         System.out.println(result);
     }
