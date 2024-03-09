8ed5201b044b258386dfe579b442bf07d2874386
rockintuna <rockintuna@naver.com>
2024-03-09 22:50:05 +0900

    leatcode 2540 two pointer

---
+++ src/main/java/leetcode/Solution2540.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class Solution2540 {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int index1 = 0;
+        int index2 = 0;
+
+        while ( index1 < nums1.length && index2 < nums2.length ) {
+            if ( nums1[index1] == nums2[index2] ) {
+                return nums1[index1];
+            } else if ( nums1[index1] < nums2[index2] ) {
+                index1++;
+            } else {
+                index2++;
+            }
+        }
+        return -1;
+    }
+}
