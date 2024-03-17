c428a0bb4bcc3044c632482ffe63c513a339554b
rockintuna <rockintuna@naver.com>
2024-03-17 23:09:23 +0900

    leatcode 349 using set O(N)

---
+++ src/main/java/leetcode/Solution349.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int i = 0; i < nums1.length; i++) {
+            set1.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            set2.add(nums2[i]);
+        }
+
+        List<Integer> union = new ArrayList<>();
+        for (Integer integer : set1) {
+            if ( set2.contains(integer) ) {
+                union.add(integer);
+            }
+        }
+
+        int[] result = new int[union.size()];
+        for (int i = 0; i < union.size(); i++) {
+            result[i] = union.get(i);
+        }
+        return result;
+    }
+}
